d5dda2f184f37b60fcc780ff908dbafd3b9f8759
Sofia1316 <sofiiaa1316@gmail.com>
2025-03-03 20:42:08 +0100

    dia 2

--- udemy/100dias_Python_Pro_Bootcamp/02_dia_calculadora.py
+++ udemy/100dias_Python_Pro_Bootcamp/02_dia_calculadora.py
@@ -1,1 +1,32 @@
-print("Hola"[0]) # Esto imprime 
+# En función de la posición
+print("Hola"[0]) # H
+
+# Concadenadas
+print("123" + "456") # 123456
+
+# Operaciones de valores
+print(123 + 456) # 468s
+
+# Enteros largos
+print(123_456_789)
+
+# Float
+print(3.1415)
+
+#len(12345) dará error ya que este no trabaja con enteros
+len("Hola")
+
+# Para comprobar el tipo de dato
+print(type("Hola")) # class str (array)
+
+# Para capitalizar de str a int
+print(int("123") + int("456")) # 579
+
+# Para redondear números
+print(round(3.9)) # 4
+
+# Si quiero concatenar cadena con números 
+edad = 12
+print(f"Tienes {edad} años")
+
+# PROYECTO 2
